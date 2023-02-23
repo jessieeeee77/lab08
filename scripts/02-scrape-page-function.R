d24fce3c1fa33a11ba52ed210856ada95d944590
jessieeeee77 <huz220@wfu.edu>
2023-02-23 12:35:03 -0500

    67

--- scripts/02-scrape-page-function.R
+++ scripts/02-scrape-page-function.R
@@ -5,23 +5,51 @@ library(rvest)
 
 # function: scrape_page --------------------------------------------------------
 
-___ <- function(url){
+scrape_page <- function(url){
   
   # read page
   page <- read_html(url)
   
-  # scrape titles
-  titles <- ___
+  # scrape titles ----------------------------------------------------------------
   
-  # scrape links
-  links <- ___
+  titles <- page %>%
+    html_nodes(".iteminfo") %>%
+    html_node("h3 a") %>%
+    html_text() %>%
+    str_squish()
   
-  # scrape artists 
-  artists <- ___
+  # scrape links -----------------------------------------------------------------
   
-  # create and return tibble
-  tibble(
-    ___
+  links <- page %>%
+    html_nodes(".iteminfo") %>%
+    html_node("h3 a") %>%
+    html_attr("href") %>%
+    str_replace("\\.", "___")
+  
+  # scrape artists ---------------------------------------------------------------
+  
+  artists <- page %>%
+    html_node(".artist") %>%
+    html_text() %>%
+    str_squish()
+  
+  # put together in a data frame -------------------------------------------------
+  
+  art_first_page <- tibble(
+    title = titles,
+    artist = artists,
+    link = links
   )
+
   
 }
+
+
+add_two <- function(x){
+  x + 2
+}
+add_two(3)
+add_two(10)
+
+scrape_page(first_url)
+scrape_page(second_url)
